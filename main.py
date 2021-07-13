d466f12c4bde1da1827bd24863e3994ca4d2ec4a
esslushy <esslushy@gmail.com>
2021-07-12 23:33:19 -0500

    got data saving to work, process should be finished

--- main.py
+++ main.py
@@ -24,27 +24,32 @@ df = df.drop(0)
 origins = list(df['zip'])
 destinations = list(df.columns)[1:] # [1:] to ignore the zip column
 
-print(origins, destinations)
-exit()
-
 # Object holding all the data
-data = {
-    'origin': {
-        'location' : 289,
-        'location2' : 239
-    }
-}
+data = {}
+
+# Saving function
+def save_data(data, save_file):
+    distances_df = pd.DataFrame.from_dict(data, orient='index')
+    # distances_df.to_excel('data.xlsx')
+    distances_df.to_csv(save_file, sep=',')
 
 # Loop through each origin and mark its connection to each destination
 for origin in origins:
+    # Add origin to data set
+    data[origin] = {}
     for destination in destinations:
         # Process destination to replace all spaces with html safe characters
-        destination = destination.replace(' ', '%20')
+        html_destination = destination.replace(' ', '%20')
         # Get Data from website
-        driver.get(host + f'?&from={origin}&to={destination}')
+        driver.get(host + f'?&from={origin}&to={html_destination}')
         distances = driver.find_element_by_id('driving_status')
         # Rest for a bit so that the update can occur
-        time.sleep(1.5)
+        time.sleep(3)
         # Pull out miles
         miles = re.search(r'\d*\.?\d+', distances.text).group()
-        print(miles)
+        # Add miles under destination label for origin
+        data[origin][destination] = miles
+    # Make a backup csv of each origin every time you go through
+    save_data(data, 'data_partial.csv')
+# Make the final one
+save_data(data, 'data_final.csv')
